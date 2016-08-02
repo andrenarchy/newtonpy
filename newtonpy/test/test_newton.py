287ce573b60825af638f280b36531a89c3b49adf
André Gaul <andre@gaul.io>
2016-08-02 12:54:17 +0200

    initial commit

---
+++ newtonpy/test/test_newton.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class NewtonTest(unittest.TestCase):
+    def test_sin(self):
+        print("test_sin")
+        # TEST CODE
+
+    def test_polynomial(self):
+        print("test_sin")
